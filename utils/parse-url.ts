2ebaebb843c473ed4350116f62933e949991646a
Anatoly Pitikin <xapdkop@xapdkop.space>
2020-09-02 23:22:09 +0300

    v2.0 release
    - Fix text color for light theme
    - Add table sort by columns
    - Trim url params when parsing url
    - Some optimizations for transformer

--- utils/parse-url.ts
+++ utils/parse-url.ts
@@ -1,7 +1,12 @@
+function trim(url: string): string {
+    const i = url.indexOf('?');
+    return ~i ? url.slice(0, i) : url;
+}
+
 export default function (path: string): { items: { text: string, href: string }[], last?: string, title: string } {
     const items: { text: string, href: string }[] = [];
     let title = '/', link = '/';
-    for (const part of path.split('/')) {
+    for (const part of trim(path).split('/')) {
         if (part) {
             const text = decodeURIComponent(part);
             link += part + '/';
--- utils/transformer.ts
+++ utils/transformer.ts
@@ -4,22 +4,37 @@ import { extname } from 'path';
 import navigator from './parse-url';
 import typer from './ext2type';
 
+function text(element: CheerioElement): string {
+    return element?.firstChild?.data ?? '';
+}
+
+function href(element: CheerioElement): string {
+    return element?.firstChild?.attribs['href'] ?? '';
+}
+
 export default function (data: string, url: string): any {
     const $ = load(data);
     const items = [];
-    for (const element of $('body>table>tbody>tr').slice(3, -1).toArray()) {
+    const rows = $('body>table>tbody>tr').toArray();
+    const header = rows[0].children;
+    const sort = {
+        name: href(header[1]),
+        date: href(header[2]),
+        size: href(header[3])
+    };
+    for (const element of rows.slice(3, -1)) {
         const row = element.children;
-        const link = $(row[1].firstChild).attr('href') ?? '';
+        const link = href(row[1]);
         const isFolder = link[link.length - 1] === '/';
-        const name = $(row[1].firstChild).text();
+        const name = text(row[1].firstChild);
         items.push({
             type: isFolder ? 'folder' : typer(extname(link)),
             link: link,
             name: isFolder ? name.slice(0, -1) : name,
-            date: $(row[2]).text(),
-            size: $(row[3]).text()
+            date: text(row[2]),
+            size: text(row[3])
         });
     }
     const { items: navigation, title, last } = navigator(url);
-    return { rows: items, navigation, title, last };
+    return { rows: items, navigation, title, last, sort };
 }
