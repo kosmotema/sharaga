edfb24889dc727e9bc3dedd594eff4355e80d87e
Anatoly Pitikin <git@xapdkop.dev>
2021-08-10 01:00:11 +0300

    improvements to compression and encoding
    - support of chain (de)compression [if order of decompressors is right]
    - parse encoding from charset and convert to node's variant if possible
    - return original page if compression or encoding is not supported
    - move decompression and decoding to separate files
    
    resolves #17

---
+++ utils/decompress.ts
@@ -0,0 +1,29 @@
+import { Transform } from 'stream';
+import { createGunzip, createBrotliDecompress, createInflate } from 'zlib';
+
+export default function (header: string) {
+    const encodings = header.split(',').map(s => s.trim());
+    let decompress: Transform | undefined;
+
+    for (const encoding of encodings) {
+        let target: Transform;
+        switch (encoding) {
+            case 'gzip':
+            case 'x-gzip':
+                target = createGunzip();
+                break;
+            case 'deflate':
+                target = createInflate();
+                break;
+            case 'br':
+                target = createBrotliDecompress();
+                break;
+            default:
+                return undefined;
+        }
+
+        decompress = decompress ? target.pipe(decompress) : target;
+    }
+
+    return decompress;
+}
--- utils/proxify.ts
+++ utils/proxify.ts
@@ -1,24 +1,25 @@
 import { FastifyInstance } from 'fastify';
 
-import { createGunzip, createBrotliDecompress, createInflate } from 'zlib';
 import { get as httpGet } from 'http';
 import { get as httpsGet } from 'https';
 import createError from 'http-errors';
 
 import transform from './transformer';
 import params from './params';
+import decompress from './decompress';
+import { Readable } from 'stream';
+import { decode, toBufferEncoding } from './decode';
 
 const get = params.protocol === 'https' ? httpsGet : httpGet;
 const info = {
     origin: params.url.toString(),
     auth: params.auth,
-    charset: params.encoding as BufferEncoding ?? 'utf-8'
-}
+    charset: (params.encoding as BufferEncoding) ?? 'utf-8'
+};
 
 export default function (server: FastifyInstance) {
-    return server.get('*', async function (request, reply) {
+    return server.get('*', function (request, reply) {
         request.headers['host'] = params.url.hostname;
-        // request.headers['accept-encoding'] = 'gzip, deflate, br';
         request.headers['accept-charset'] = info.charset;
         const options = {
             headers: request.headers,
@@ -27,47 +28,51 @@ export default function (server: FastifyInstance) {
         get(info.origin + request.url, options, proxy => {
             const { headers, statusCode: code } = proxy;
             headers['authorization'] && delete headers['authorization'];
-            
+
             if (!code || code >= 400) {
                 proxy.resume();
                 reply.send(createError(code ?? 500));
-            }
-            else if (code >= 300 && code < 400) {
+            } else if (code >= 300 && code < 400) {
                 if (headers['location'])
-                    reply.redirect(code, headers['location'].replace(info.origin, ''));
-                else
-                    reply.send(new createError.InternalServerError());
-            }
-            else {
+                    reply.redirect(
+                        code,
+                        headers['location'].replace(info.origin, '')
+                    );
+                else reply.send(new createError.InternalServerError());
+            } else {
                 if (headers['content-type']?.startsWith('text/html')) {
-                    let data = '';
-                    let decompress = null;
+                    let stream: Readable = proxy;
 
-                    switch (headers['content-encoding']) {
-                        case 'gzip': case 'x-gzip':
-                            decompress = createGunzip();
-                            break;
-                        case 'deflate':
-                            decompress = createInflate();
-                            break;
-                        case 'br':
-                            decompress = createBrotliDecompress();
-                            break;
-                        default:
-                            proxy.resume();
-                            return reply.send(new createError.InternalServerError(`Unsupported encoding: ${headers['content-encoding']}`));
+                    const compression = headers['content-encoding'];
+                    if (compression) {
+                        const decompressor = decompress(compression);
+                        if (!decompressor) {
+                            return reply
+                                .code(code)
+                                .headers(headers)
+                                .send(proxy);
+                        }
+                        stream = stream.pipe(decompressor);
                     }
 
-                    proxy.pipe(
-                        decompress
-                            .setEncoding(info.charset)
-                            .on('data', (chunk: string) => data += chunk)
-                            .on('end', () => reply.view('dir', { style: 'table', ...transform(data, request.url) }))
+                    const bufferEncoding = toBufferEncoding(
+                        headers['content-type']
                     );
-                }
-                else
-                    reply.code(200).headers(headers).send(proxy);
+
+                    if (!bufferEncoding) {
+                        return reply.code(code).headers(headers).send(proxy);
+                    }
+
+                    decode(stream, bufferEncoding).then(data =>
+                        reply.view('dir', {
+                            style: 'table',
+                            ...transform(data, request.url)
+                        })
+                    );
+                } else reply.code(200).headers(headers).send(proxy);
             }
-        }).on('error', e => reply.send(new createError.InternalServerError(e.message)));
+        }).on('error', e =>
+            reply.send(new createError.InternalServerError(e.message))
+        );
     });
 }
