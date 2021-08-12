71556a26c4a331c2f24d578eaa27d14f438e8409
Anatoly Pitikin <git@xapdkop.dev>
2021-08-12 03:24:20 +0300

    update example pm2 config

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -2,7 +2,10 @@ module.exports = {
     apps: [
         {
             name: 'sharaga',
-            script: './app.js'
+            script: './app.js',
+            env_production: {
+                NODE_ENV: 'production'
+            }
         },
         {
             script: './service-worker/',
