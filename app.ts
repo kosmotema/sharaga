c003579ae8c0cae0a82c1d47d0696b5662a830bf
Anatoly Pitikin <git@xapdkop.dev>
2021-08-14 02:39:11 +0300

    get rid of partials (and format layout.hbs)
    partials are not reused so they're useless

--- app.ts
+++ app.ts
@@ -21,10 +21,6 @@ server.register(view, {
     root: join(__dirname, 'views'),
     layout: 'layout',
     options: {
-        partials: {
-            nav: './partials/nav.hbs',
-            footer: './partials/footer.hbs'
-        },
         useHtmlMinifier: minifier,
         htmlMinifierOptions: {
             removeComments: true,
