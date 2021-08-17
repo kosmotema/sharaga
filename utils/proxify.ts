ebc06295ece6f5dc967589e313ae72d170daba03
Anatoly Pitikin <git@xapdkop.dev>
2021-08-17 23:24:50 +0300

    make proxify as fastify plugin

--- utils/proxify.ts
+++ utils/proxify.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyPluginCallback, FastifyReply, FastifyRequest } from 'fastify';
 
 import { get as httpGet } from 'node:http';
 import { get as httpsGet } from 'node:https';
@@ -16,55 +16,60 @@ const info = {
   auth: parameters.auth,
 };
 
-// FIXME: use fastify pluggins syntax
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function setup(server: FastifyInstance) {
-  return server.get('*', (request, reply) => {
-    request.headers.host = parameters.url.hostname;
-    const options = {
-      headers: request.headers,
-      auth: info.auth,
-    };
-    // eslint-disable-next-line consistent-return
-    get(info.origin + request.url, options, (proxy) => {
-      const { headers, statusCode: code } = proxy;
-      if (headers.authorization) {
-        delete headers.authorization;
+function proxy(request: FastifyRequest, reply: FastifyReply) {
+  request.headers.host = parameters.url.hostname;
+  const options = {
+    headers: request.headers,
+    auth: info.auth,
+  };
+  get(info.origin + request.url, options, (upstream) => {
+    const { headers, statusCode: code } = upstream;
+    if (headers.authorization) {
+      delete headers.authorization;
+    }
+
+    if (!code || code >= 400) {
+      upstream.resume();
+      reply.send(createError(code ?? 500));
+    } else if (code >= 300 && code < 400) {
+      if (headers.location) reply.redirect(code, headers.location.replace(info.origin, ''));
+      else reply.send(new createError.InternalServerError());
+    } else if (headers['content-type']?.startsWith('text/html')) {
+      let stream: Readable = upstream;
+
+      const compression = headers['content-encoding'];
+      if (compression) {
+        const decompressor = decompress(compression);
+        if (!decompressor) {
+          reply.code(code).headers(headers).send(upstream);
+          return;
+        }
+        stream = stream.pipe(decompressor);
       }
 
-      if (!code || code >= 400) {
-        proxy.resume();
-        reply.send(createError(code ?? 500));
-      } else if (code >= 300 && code < 400) {
-        if (headers.location) reply.redirect(code, headers.location.replace(info.origin, ''));
-        else reply.send(new createError.InternalServerError());
-      } else if (headers['content-type']?.startsWith('text/html')) {
-        let stream: Readable = proxy;
+      const bufferEncoding = toBufferEncoding(headers['content-type']);
 
-        const compression = headers['content-encoding'];
-        if (compression) {
-          const decompressor = decompress(compression);
-          if (!decompressor) {
-            return reply.code(code).headers(headers).send(proxy);
-          }
-          stream = stream.pipe(decompressor);
-        }
+      if (!bufferEncoding) {
+        reply.code(code).headers(headers).send(upstream);
+        return;
+      }
 
-        const bufferEncoding = toBufferEncoding(headers['content-type']);
+      decode(stream, bufferEncoding)
+        .then((data) =>
+          reply.view('dir', {
+            style: 'table',
+            ...transform(data, request.url),
+          })
+        )
+        .catch((error) => new createError.InternalServerError(error?.message));
+    } else reply.code(code).headers(headers).send(upstream);
+  }).on('error', (error) => reply.send(new createError.InternalServerError(error.message)));
+};
 
-        if (!bufferEncoding) {
-          return reply.code(code).headers(headers).send(proxy);
-        }
+const setup: FastifyPluginCallback = (fastify, _, done) => {
+  fastify.get('*', proxy);
 
-        decode(stream, bufferEncoding)
-          .then((data) =>
-            reply.view('dir', {
-              style: 'table',
-              ...transform(data, request.url),
-            })
-          )
-          .catch((error) => new createError.InternalServerError(error?.message));
-      } else reply.code(200).headers(headers).send(proxy);
-    }).on('error', (error) => reply.send(new createError.InternalServerError(error.message)));
-  });
-}
+  done();
+};
+
+export default setup;
